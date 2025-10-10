78d8f8edfad7c257bacdd6f46e0c8b70862bf6d6
nichi2000 <nichikah.com@gmail.com>
2025-10-10 16:49:38 +0900

    プレビュー表示

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var enableHighlightCheckbox = document.getElementById('enableHighlight');
+    var enablePreviewCheckbox = document.getElementById('enablePreview');
 
     // 初期状態を読み込む
-    chrome.storage.sync.get('highlightEnabled', function(data) {
+    chrome.storage.sync.get(['highlightEnabled', 'previewEnabled'], function(data) {
         enableHighlightCheckbox.checked = data.highlightEnabled !== undefined ? data.highlightEnabled : false; // デフォルトはOFF
+        enablePreviewCheckbox.checked = data.previewEnabled !== undefined ? data.previewEnabled : false; // デフォルトはOFF
+        // enablePreviewCheckbox.disabled = !enableHighlightCheckbox.checked; // 削除
     });
 
     // チェックボックスの状態が変更されたら保存し、コンテンツスクリプトにメッセージを送信
@@ -14,6 +17,15 @@ document.addEventListener('DOMContentLoaded', function() {
         // 現在のタブにメッセージを送信
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {action: "toggleHighlight", enabled: isEnabled});
+        });
+    });
+
+    enablePreviewCheckbox.addEventListener('change', function() {
+        var isEnabled = this.checked;
+        chrome.storage.sync.set({'previewEnabled': isEnabled});
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "togglePreview", enabled: isEnabled});
         });
     });
 });
